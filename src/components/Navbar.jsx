17c7025c49db4f0459110952be754fb1569c9856
Katarzyna <k.dw@poczta.onet.pl>
2024-10-27 17:47:13 +0100

    Changed cart icon in nav in the way, that shows number of items in the cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import useStore from '../stores/store';
 const Navbar = () => {
   const cart = useStore((state) => state.cart);
 
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
         <nav className="bg-custom-pink text-custom-dark p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
@@ -13,7 +15,7 @@ const Navbar = () => {
         <Link to="/cart" className="flex items-center space-x-2">
           <span>Cart</span>
           <span className="bg-custom-cta text-white rounded-full px-3 py-1 text-sm">
-            {cart.length}
+            {totalItems}
           </span>
         </Link>
       </div>
